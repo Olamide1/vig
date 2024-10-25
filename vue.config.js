1a6446a7a4af307b68c7c2cfe89dd60192d2884d
Olamide1 <olamideakomolafe1234@gmail.com>
2024-10-25 22:17:55 +0100

    included sitemap for SEO work

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+// Paths from your router
+const paths = [
+  { path: '/', lastmod: new Date().toISOString(), priority: 1.0 },
+  { path: '/cancel', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/dashboard', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/feedback', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/pricing', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/profile', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/roadmap', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/success', lastmod: new Date().toISOString(), priority: 0.8 },
+  { path: '/login', lastmod: new Date().toISOString(), priority: 0.7 },
+  { path: '/about', lastmod: new Date().toISOString(), priority: 0.7 }
+];
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({ base: 'https://simplevig.com', paths })
+    ],
+  },
+});
